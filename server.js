9a87ec14ee4545843e08205fa2d82db9980f60ed
Rik Helsen <rik_helsen@hotmail.com>
2021-11-08 00:47:40 +0100

    ⚡ Only Try-Catch `send` methods

--- server.js
+++ server.js
@@ -16,40 +16,40 @@ console.log("Discord channel id: " + DISCORD_CHANNEL_ID);
 
 // Discord -> Telegram handler
 discordClient.on("message", message => {
+
+	// the program currently check if the message's from a bot to check for duplicates.
+	// This isn't the best method but it's good enough.
+	// A webhook counts as a bot in the discord api, don't ask me why.
+	// Ignore messages from bots if DISCORD_FORWARD_BOT is 'false'
+	if (message.channel.id !== DISCORD_CHANNEL_ID || (message.author.bot && !DISCORD_FORWARD_BOT)) {
+		return;
+	}
+
+	let mentioned_usernames = []
+	for (let mention of message.mentions.users) {
+		mentioned_usernames.push("@" + mention[1].username);
+	}
+	var attachmentUrls = []
+	for (let attachment of message.attachments) {
+		attachmentUrls.push(attachment[1].url);
+	}
+
+	// attachmentUrls is empty when there are no attachments so we can be just lazy
+	var finalMessageContent = message.content.replace(/<@.*>/gi, '');
+	// convert bold text for telegram markdown
+	finalMessageContent = finalMessageContent.replace(/\*\*/g, '*');
+
+	var text = `*\[DISCORD\] ${message.author.username} (${message.author.username}#${message.author.discriminator}):*\n`;
+	text += finalMessageContent
+	text += ` ${attachmentUrls.join(' ')}`;
+	text += mentioned_usernames.join(" ");
+
 	try {
-		// the program currently check if the message's from a bot to check for duplicates.
-		// This isn't the best method but it's good enough.
-		// A webhook counts as a bot in the discord api, don't ask me why.
-		// Ignore messages from bots if DISCORD_FORWARD_BOT is 'false'
-		if (message.channel.id !== DISCORD_CHANNEL_ID || (message.author.bot && !DISCORD_FORWARD_BOT)) {
-			return;
-		}
-
-		let mentioned_usernames = []
-		for (let mention of message.mentions.users) {
-			mentioned_usernames.push("@" + mention[1].username);
-		}
-		var attachmentUrls = []
-		for (let attachment of message.attachments) {
-			attachmentUrls.push(attachment[1].url);
-		}
-
-		// attachmentUrls is empty when there are no attachments so we can be just lazy
-		var finalMessageContent = message.content.replace(/<@.*>/gi, '');
-		// convert bold text for telegram markdown
-		finalMessageContent = finalMessageContent.replace(/\*\*/g, '*');
-
-		var text = `*\[DISCORD\] ${message.author.username} (${message.author.username}#${message.author.discriminator}):*\n`;
-		text += finalMessageContent
-		text += ` ${attachmentUrls.join(' ')}`;
-		text += mentioned_usernames.join(" ");
-
-
-			telegram.sendMessage({
-				chat_id: TELEGRAM_CHAT_ID,
-				text: text,
-				parse_mode: 'markdown'
-			});
+		telegram.sendMessage({
+			chat_id: TELEGRAM_CHAT_ID,
+			text: text,
+			parse_mode: 'markdown'
+		});
 	}
 	catch(err) {
 		console.log(err.message);
@@ -59,62 +59,63 @@ discordClient.on("message", message => {
 
 // Telegram -> Discord handler
 telegram.on("message", async function (message) {
+
+	// console.log(message)
+	if (message.chat.id != TELEGRAM_CHAT_ID) {
+		return;
+	}
+
+	// Ignore messages from bots
+	if (message.from.is_bot) {
+		return;
+	}
+
+	var username = `[TELEGRAM] ${message.from.first_name}`;
+	if (message.from.last_name) {
+		username += ` ${message.from.last_name}`;
+	}
+	if (message.from.username) {
+		username += ` (@${message.from.username})`;
+	}
+
+	let profileUrl = await telegramGetProfilePic(message);
+
+	var text;
+	var fileId;
+
+	if (!message.document && !message.photo && !message.sticker) {
+		if (!message.text) {
+			return;
+		}
+		text = message.text;
+
+		// convert bold, italic & hyperlink Telegram text for Discord markdown
+		if (message.entities) {
+			text = convert_text_telegram_to_discord(text, message.entities);
+		}
+
+	} else {
+		text = message.caption;
+
+		// convert bold, italic & hyperlink Telegram text for Discord markdown
+		if (message.caption_entities) {
+			text = convert_text_telegram_to_discord(text, message.caption_entities);
+		}
+
+		if (message.document) {
+			fileId = message.document.file_id;
+		} else if (message.sticker) {
+			fileId = message.sticker.file_id;
+		} else if (message.photo) {
+			fileId = message.photo[2].file_id;
+		}
+	}
+
+	if (text) {
+		text = text.replace(/@everyone/g, "[EVERYONE]").replace(/@here/g, "[HERE]");
+	}
+
 	try {
-		// console.log(message)
-		if (message.chat.id != TELEGRAM_CHAT_ID) {
-			return;
-		}
-
-		// Ignore messages from bots
-		if (message.from.is_bot) {
-			return;
-		}
-
-		var username = `[TELEGRAM] ${message.from.first_name}`;
-		if (message.from.last_name) {
-			username += ` ${message.from.last_name}`;
-		}
-		if (message.from.username) {
-			username += ` (@${message.from.username})`;
-		}
-
-		let profileUrl = await telegramGetProfilePic(message);
-
-		var text;
-		var fileId;
-
-		if (!message.document && !message.photo && !message.sticker) {
-			if (!message.text) {
-				return;
-			}
-			text = message.text;
-
-			// convert bold, italic & hyperlink Telegram text for Discord markdown
-			if (message.entities) {
-				text = convert_text_telegram_to_discord(text, message.entities);
-			}
-
-		} else {
-			text = message.caption;
-
-			// convert bold, italic & hyperlink Telegram text for Discord markdown
-			if (message.caption_entities) {
-				text = convert_text_telegram_to_discord(text, message.caption_entities);
-			}
-
-			if (message.document) {
-				fileId = message.document.file_id;
-			} else if (message.sticker) {
-				fileId = message.sticker.file_id;
-			} else if (message.photo) {
-				fileId = message.photo[2].file_id;
-			}
-		}
-
-		if (text) {
-			text = text.replace(/@everyone/g, "[EVERYONE]").replace(/@here/g, "[HERE]");
-		}
-
 		if (!fileId) {
 			await discordWebhookClient.send(text, {
 				username: username,
