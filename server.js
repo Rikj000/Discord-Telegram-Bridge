c090ef9709f98dc36b6adafb73df5b780d34c57d
Rik Helsen <rik_helsen@hotmail.com>
2021-11-07 10:53:17 +0100

    ⚡ Telegram - Aligned Username format with Discord️

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ discordClient.on("message", message => {
 	// attachmentUrls is empty when there are no attachments so we can be just lazy
 	var finalMessageContent = message.content.replace(/<@.*>/gi, '');
 
-	var text = `[DISCORD] ${message.author.username}#${message.author.discriminator}: `;
+	var text = `**[DISCORD] ${message.author.username} (${message.author.username}#${message.author.discriminator}):**\n`;
 	text += finalMessageContent
 	text += ` ${attachmentUrls.join(' ')}`;
 	text += mentioned_usernames.join(" ");
